e1d572c6e03a57a2992bf07b180c6ac558399db9
Ismail SEZEN <613356+isezen@users.noreply.github.com>
2020-03-23 11:13:40 +0300

    update

--- data-raw/dataset.R
+++ data-raw/dataset.R
@@ -12,19 +12,6 @@ for (d in c("dw", "ra", "jh")) {
 
 cn <- c("date", "country_region", "province_state", "type", "cases",
         "lat", "long")
-add.row <- function(date, type, cases, ntest) {
-  df <- data.frame(date, "Turkey", NA, type, cases, ntest, 38.9637, 35.2433)
-  colnames(df) <- cn
-  c19tr <- rbind(c19tr, df)
-  return(c19tr)
-}
-
-# c19tr <- c19jh[c19jh$country_region == "Turkey",]
-# c19tr <- c19tr[order(c19tr$date, c19tr$type),]
-# c19tr$ntest <- NA
-# c19tr <- c19tr[,cn]
-# rownames(c19tr) <- NULL
-# c19tr[c19tr$type == "Confirmed", "ntest"][9:10] <- c(1981, 5637)
 filename <- 'data-raw/c19trw.csv'
 c19trw <- read.csv2(filename, na.strings = "",
                    colClasses = c("POSIXct", "integer", "integer",
@@ -36,6 +23,9 @@ c19trl$lat <- 38.9637
 c19trl$long <- 35.2433
 c19trl <- c19trl[,cn]
 c19trl <- c19trl[order(c19trl$date, c19trl$type),]
+c19trl$country_region <- factor(c19trl$country_region)
+c19trl$province_state <- factor(c19trl$province_state)
+c19trl$type <- factor(c19trl$type)
 
 write.csv2(c19trl, "data-raw/c19trl.csv", row.names = FALSE, quote = FALSE, na = '')
 use_data(c19trw, c19trl, overwrite = TRUE)
