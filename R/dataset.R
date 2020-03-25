71dac226578dcfab839b96f95f7d7889515d3412
Ismail SEZEN <613356+isezen@users.noreply.github.com>
2020-03-26 01:03:37 +0300

    update

--- R/dataset.R
+++ R/dataset.R
@@ -2,51 +2,40 @@
 #'
 #' @details Covid-19 datasets from various sources
 #'
-#' [dataset]_w is wide format
+#' [dataset]w is wide format
 #'
-#' [dataset]_l is long format
+#' [dataset]l is long format
 #'
 #' @usage NULL
-#' @description \code{c19dw_w}: Data from data.world (Wide format)
+#' @description \code{c19dww}: Data from data.world (Wide format)
 #' @rdname dataset
 #' @source \code{c19dw}: \url{https://query.data.world}
-"c19dw_w"
+"c19dww"
 
 #' @usage NULL
-#' @description \code{c19dw_l}: Data from data.world (Long format)
+#' @description \code{c19dwl}: Data from data.world (Long format)
 #' @rdname dataset
-"c19dw_l"
+"c19dwl"
 
 #' @usage NULL
-#' @description \code{c19ra_w}: Data from RamiKrispin GitHub repository (Wide format)
-#' @rdname dataset
-#' @source \code{c19ra}: \url{https://raw.githubusercontent.com/RamiKrispin/coronavirus-csv/master/coronavirus_dataset.csv}
-"c19ra_w"
-
-#' @usage NULL
-#' @description \code{c19ra_l}: Data from RamiKrispin GitHub repository (Long format)
+#' @description \code{c19raw}: Data from RamiKrispin GitHub repository (Wide format)
 #' @rdname dataset
 #' @source \code{c19ra}: \url{https://raw.githubusercontent.com/RamiKrispin/coronavirus-csv/master/coronavirus_dataset.csv}
-"c19ra_l"
+"c19raw"
 
 #' @usage NULL
-#' @description \code{c19jh_w}: Data from John Hopkins University (Wide format)
+#' @description \code{c19ral}: Data from RamiKrispin GitHub repository (Long format)
+#' @rdname dataset
+#' @source \code{c19ra}: \url{https://raw.githubusercontent.com/RamiKrispin/coronavirus-csv/master/coronavirus_dataset.csv}
+"c19ral"
+
+#' @usage NULL
+#' @description \code{c19jhw}: Data from John Hopkins University (Wide format)
 #' @rdname dataset
 #' @source \code{c19jh}: \url{https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/}
-"c19jh_w"
+"c19jhw"
 
 #' @usage NULL
-#' @description \code{c19jh_l}: Data from John Hopkins University (Long format)
+#' @description \code{c19jhl}: Data from John Hopkins University (Long format)
 #' @rdname dataset
-"c19jh_l"
-
-#' @usage NULL
-#' @description \code{c19tr_w}: Data for Turkey (Wide format)
-#' @rdname dataset
-#' @source \code{c19tr}: \url{https://twitter.com/drfahrettinkoca}
-"c19tr_w"
-
-#' @usage NULL
-#' @description \code{c19tr_l}: Data for Turkey (Long format)
-#' @rdname dataset
-"c19tr_l"
+"c19jhl"
--- data-raw/dataset.R
+++ data-raw/dataset.R
@@ -1,56 +1,38 @@
 library(usethis)
 source("R/download.R")
 
+c19raw <- c19dww <- c19jhw <- NULL
+c19ral <- c19dwl <- c19jhl <- NULL
 # download from scratch
 for (d in c("dw", "ra", "jh")) {
   data_name <- paste0("c19", d)
-  data_name <- paste0(data_name, ifelse(d == "jh", "_w", "_l"))
+  data_name <- paste0(data_name, ifelse(d == "jh", "w", "l"))
   eval(parse(text = paste0(data_name, "<- download.c19('", d, "')")))
   eval(parse(text = sprintf("use_data(%s, overwrite = TRUE)", data_name)))
 }
 
 # long format for John-Hopkins data
-cn <- colnames(c19jh_w)[8:11]
+cn <- colnames(c19jhw)[8:11]
 substring(cn, 1) <- toupper(substring(cn, 1, 1))
-colnames(c19jh_w)[8:11] <- cn
-c19jh_l <- reshape2::melt(c19jh_w,
+colnames(c19jhw)[8:11] <- cn
+c19jhl <- reshape2::melt(c19jhw,
                       measure.vars = c("Confirmed", "Deaths", "Recovered", "Active"),
                       variable.name = "type", value.name = "cases")
-c19jh_l <- with(c19jh_l, c19jh_l[order(country_region, province_state,
+c19jhl <- with(c19jhl, c19jhl[order(country_region, province_state,
                                        admin2, date, type),])
 
 # wide format for data.world
-c19dw_w <- reshape2::dcast(c19dw_l, date + country_region + province_state +
+c19dww <- reshape2::dcast(c19dwl, date + country_region + province_state +
                              lat + long ~ type, value.var = "cases")
-c19dw_w <- with(c19dw_w, c19dw_w[order(country_region, province_state, date),])
-c19ra_w <- reshape2::dcast(c19ra_l, date + country_region + province_state +
+c19dww <- with(c19dww, c19dww[order(country_region, province_state, date),])
+c19raw <- reshape2::dcast(c19ral, date + country_region + province_state +
                              lat + long ~ type, value.var = "cases")
-c19ra_w <- with(c19ra_w, c19ra_w[order(country_region, province_state, date),])
-use_data(c19jh_l, c19dw_w, c19ra_w, overwrite = TRUE)
+c19raw <- with(c19raw, c19raw[order(country_region, province_state, date),])
+use_data(c19jhl, c19dww, c19raw, overwrite = TRUE)
 # -------------
 
-cn <- c("date", "country_region", "province_state", "type", "cases",
-        "lat", "long")
-filename <- 'data-raw/c19tr_w.csv'
-c19tr_w <- read.csv2(filename, na.strings = "",
-                   colClasses = c("POSIXct", "integer", "integer",
-                                "integer", "integer"))
-c19tr_l <- reshape2::melt(c19tr_w, id.vars = c("date"), variable.name = "type", value.name = "cases")
-c19tr_l$country_region <- "Turkey"
-c19tr_l$province_state <- NA
-c19tr_l$lat <- 38.9637
-c19tr_l$long <- 35.2433
-c19tr_l <- c19tr_l[,cn]
-c19tr_l <- c19tr_l[order(c19tr_l$date, c19tr_l$type),]
-c19tr_l$country_region <- factor(c19tr_l$country_region)
-c19tr_l$province_state <- factor(c19tr_l$province_state)
-c19tr_l$type <- factor(c19tr_l$type)
-
-# write.csv2(c19tr_l, "data-raw/c19tr_l.csv", row.names = FALSE, quote = FALSE, na = '')
-use_data(c19tr_w, c19tr_l, overwrite = TRUE)
-
 cmd <- "write.csv2(%s, 'data-raw/%s.csv', row.names = FALSE, quote = FALSE, na = '')"
-for (data_name in list("c19dw_w", "c19ra_w", "c19jh_w")) {
+for (data_name in list("c19dww", "c19raw", "c19jhw")) {
   eval(parse(text = sprintf(cmd, data_name, data_name)))
 }
 
