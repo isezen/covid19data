8a9b1235bc83d05c4499bca90b1e1ecf5fc307e6
Ismail SEZEN <613356+isezen@users.noreply.github.com>
2020-04-09 08:44:29 +0300

    update

--- R/dataset.R
+++ R/dataset.R
@@ -3,17 +3,22 @@
 #' @description
 #' Covid-19 datasets various resources.
 #'
-#' \code{c19jh}: John-Hopkins University
+#' \code{c19}: John-Hopkins University
 #'
 #' @details
+#' Some countries which has provinces were seperated from the original data source.
+#' These countries are Autralia, Canada, China, Denmark, France, United Kingdom
+#' and Netherlands. You can see long format of these data sources by adding
+#' 3-letters iso code at the end of 'c19.' string i.e., 'c19.gbr'.
+#'
 #' \code{[dataset_name]w}: wide format
 #'
 #' \code{[dataset_name]l}: long format
 #'
-#' @usage c19jhw # wide format
+#' @usage c19w # wide format
 #' @source \url{https://raw.githubusercontent.com/CSSEGISandData/COVID-19/}
-"c19jhw"
+"c19"
 
-#' @usage c19jhl # long format
-#' @rdname c19jhw
-"c19jhl"
+#' @usage c19l # long format
+#' @rdname c19
+"c19l"
--- data-raw/dataset.R
+++ data-raw/dataset.R
@@ -1,24 +1,39 @@
 library(usethis)
 source("R/download.R")
 
-c19raw <- c19dww <- c19jhw <- NULL
-c19ral <- c19dwl <- c19jhl <- NULL
-# download from scratch
-for (d in c("jh")) {
-  data_name <- paste0("c19", d, "l")
-  eval(parse(text = paste0(data_name, "<- download.c19('", d, "')")))
+url_lookup <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/UID_ISO_FIPS_LookUp_Table.csv"
+col_colasses <- c("integer", "factor",  "factor", "factor", "factor",
+                  "factor", "factor", "factor", "numeric", "numeric",
+                  "factor", "integer")
+lookup <- read.csv(url_lookup, colClasses = col_colasses)
+use_data(lookup, overwrite = TRUE)
+
+# c19jhw <- c19jhl <- NULL
+df <- download.c19("jh")
+df2 <- df[!duplicated(df[,2:3]), 2:3]
+names <- unique(df2[duplicated(df2[,1]), 1])
+i <- lookup[which(lookup$Country_Region %in% names & lookup$Province_State == ""),]
+i <- i[order(i$Country_Region),]
+snames <- tolower(i$iso3)
+
+for (i in 1:length(names)) {
+  data_name <- tolower(paste0("c19.", gsub("[[:space:]]", "", snames[i])))
+  df3 <- df[df$country_region == names[i],]
+  eval(parse(text = paste0(data_name, "<- df[df$country_region == names[i],]")))
   eval(parse(text = sprintf("use_data(%s, overwrite = TRUE)", data_name)))
 }
 
+c19l <- aggregate(cases ~ type + date + country_region, df, sum)
+c19l <- c19l[,colnames(df)[-c(3:5)]]
+
 # Wide format for John-Hopkins data
-c19jhw <- reshape2::dcast(c19jhl, date + country_region + province_state +
-                            lat + long ~ type, value.var = "cases")
-colnames(c19jhw) <- tolower(colnames(c19jhw))
-use_data(c19jhw, overwrite = TRUE)
+c19 <- reshape2::dcast(c19l, date + country_region ~ type, value.var = "cases")
+colnames(c19) <- tolower(colnames(c19))
+use_data(c19l, c19, overwrite = TRUE)
 # -------------
 
 cmd <- "write.csv2(%s, 'data-raw/%s.csv', row.names = FALSE, quote = FALSE, na = '')"
-for (data_name in c("c19jhw")) {
+for (data_name in c("c19")) {
   eval(parse(text = sprintf(cmd, data_name, data_name)))
 }
 
